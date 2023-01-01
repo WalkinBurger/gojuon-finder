90bdbc5b3e3918ac1d498e500ac8e7c39214ebbf
WalkinBurger <nchunghim@gmail.com>
2023-01-01 02:32:03 +0000

    Bug fix and better output readability

--- gojuon-finder.py
+++ gojuon-finder.py
@@ -19,21 +19,23 @@ for i in os.listdir("audio"): # loop through the audio file
     iExt = os.path.splitext(i)
     if iExt[1] == ".mp3":
         AudioSegment.from_mp3(file).export(f"audio/conversion/{iExt[0]}.wav", format="wav")
-        file = f"audio/{iExt[0]}.wav" # export a new converted version of audio to audio/conversion/
+        file = f"audio/conversion/{iExt[0]}.wav" # export a new converted version of audio to audio/conversion/
         ExtT = True
     elif iExt[1] == ".mp4":
         AudioSegment.from_file(file, "mp4").export(f"audio/conversion/{iExt[0]}.wav", format="wav")
-        file = f"audio/{iExt[0]}.wav" # export a new converted version of audio to audio/conversion/
+        file = f"audio/conversion/{iExt[0]}.wav" # export a new converted version of audio to audio/conversion/
         ExtT = True
     elif iExt[1] == ".wav":
-        AudioSegment.from_file(file).export(f"audio/conversion/{iExt[0]}.wav", format="wav")
-        ExtT = True # export a new converted version of audio to audio/conversion/
+        ExtT = True
     elif os.path.basename(i) != "chunks" and os.path.basename(i) != "conversion":
-        print(f"[{i}] not supported file type") # skip that wrong extension file
+        with open("outputs.txt","a", encoding="utf-8") as f:
+            f.write(f"[{i}] not supported file type\n\n") # skip that wrong extension file
 
     # start speech recognition if file extension is right
     if ExtT == True: 
         audioA = AudioSegment.from_file(file)
+        with open("outputs.txt","a", encoding="utf-8") as f:
+            f.write(f"\n[{i}] :")
         for audioC in make_chunks(audioA, 15000): # make audio into chunks
             chunkN[1] += 15 # keep track of the Nth chunk (time)
             if chunkN[1] == 60:
@@ -62,10 +64,10 @@ for i in os.listdir("audio"): # loop through the audio file
                 type(audioR)
                 try:
                     with open("outputs.txt","a", encoding="utf-8") as f:
-                        f.write("".join((f"[{i}] {chunkT} : \n", repr(r.recognize_google(audioR, language="zh-TW(cmn-Hant-TW)")), "\n\n")))
+                        f.write(" | ".join((f"\n{chunkT}", repr(r.recognize_google(audioR, language="zh-TW(cmn-Hant-TW)")))))
                 except sr.UnknownValueError:
                     with open("outputs.txt","a", encoding="utf-8") as f:
-                        f.write(f"cannot recognize audio from file: [{i}] in {chunkT}")
+                        f.write(f"\n\n{chunkT} | CANNOT RECOGNIZE SPEECH\n")
 
     # reset vars
     chunkN, chunkNE, chunkNS = [0,0], [0,0], [0,-15]
